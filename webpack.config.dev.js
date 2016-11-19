a4b7475c897786e654dd3826ec085fb23bb9ef64
Danny Miller <edaloicaro18@gmail.com>
2016-11-19 11:27:40 -0500

    modules 1-8

---
+++ webpack.config.dev.js
@@ -0,0 +1,25 @@
+import path from 'path';
+
+//https://github.com/webpack/webpack/issues/2145
+
+export default {
+  debug: true,
+  devtool: 'inline-source-map',
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+}
