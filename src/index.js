9f6abd5e57df7a4a73a68d42a61c6fa52f27ad21
Danny Miller <edaloicaro18@gmail.com>
2016-11-19 15:43:58 -0500

    Module 10 - Fech and Mocking APIs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 
-import {getUsers} from './api/userApi';
+import { getUsers, deleteUser } from './api/userApi';
 
-getUsers().then(result=>{
-  let usersBody="";
+getUsers().then(result => {
+  let usersBody = "";
 
-  result.forEach(user=>{
-    usersBody+=`<tr>
+  result.forEach(user => {
+    usersBody += `<tr>
       <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
       <td>${user.id}</td>
       <td>${user.firstName}</td>
@@ -14,5 +14,24 @@ getUsers().then(result=>{
     </tr>`
   });
 
-  global.document.getElementById('users').innerHTML=usersBody;
+  global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  console.log("Links to delete: " + deleteLinks.length);
+
+  // Must use array.from to create a real array from DOM collection
+  // getElementByClassName only returns an "array like" object
+
+  Array.from(deleteLinks, link => {
+    link.onclick = function (event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes['data-id'].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+
+
 });
